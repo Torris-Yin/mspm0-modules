44a36c63e5c440714a5b1e104b935aff54a149bb
Torris Yin <torris.yin@gmail.com>
2025-06-02 11:35:31 +0800

    Add MPU6050 and OLED hardware I2C example

---
+++ Examples/mpu6050-oled-hardware-i2c/Drivers/MPU6050/mspm0_i2c.h
@@ -0,0 +1,14 @@
+#ifndef _MSPM0_I2C_H_
+#define _MSPM0_I2C_H_
+
+int mspm0_i2c_write(unsigned char slave_addr,
+                     unsigned char reg_addr,
+                     unsigned char length,
+                     unsigned char const *data);
+
+int mspm0_i2c_read(unsigned char slave_addr,
+                    unsigned char reg_addr,
+                    unsigned char length,
+                    unsigned char *data);
+
+#endif  /* #ifndef _MSPM0_I2C_H_ */
---
+++ Examples/mpu6050-oled-hardware-i2c/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include "clock.h"
+
+#include "mpu6050.h"
+#include "oled_software_i2c.h"
+#include "oled_hardware_i2c.h"
+#include "oled_software_spi.h"
+#include "oled_hardware_spi.h"
+
+#endif  /* #ifndef _MAIN_H_ */
