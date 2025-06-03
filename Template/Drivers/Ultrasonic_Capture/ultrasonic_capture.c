e8d358672651e196aab9783e5176807a083a9122
Torris Yin <torris.yin@gmail.com>
2025-06-04 00:25:39 +0800

    Enhance ultrasonic driver

--- Template/Drivers/Ultrasonic_Capture/ultrasonic_capture.c
+++ Template/Drivers/Ultrasonic_Capture/ultrasonic_capture.c
@@ -7,5 +7,10 @@ void Ultrasonic_Init(void)
 
 uint16_t Read_Ultrasonic(void)
 {
-    return DL_Timer_getCaptureCompareValue(CAPTURE_ULTRASONIC_INST, CAPTURE_ULTRASONIC_CC_IDX) * 0.17;
+    uint16_t distVal = DL_Timer_getCaptureCompareValue(CAPTURE_ULTRASONIC_INST, CAPTURE_ULTRASONIC_CC_IDX) * 0.17;
+
+    if(distVal > 6000)
+        distVal = 0;
+
+    return distVal;
 }
