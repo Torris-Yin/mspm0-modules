aea54cb56f12554e6f0a2309c2ec0d00106059b6
Torris Yin <torris.yin@gmail.com>
2025-06-08 23:05:23 +0800

    Improve BNO08X driver

--- Template/Drivers/MSPM0/interrupt.c
+++ Template/Drivers/MSPM0/interrupt.c
@@ -14,34 +14,34 @@ void SysTick_Handler(void)
 void UART_BNO08X_INST_IRQHandler(void)
 {
     uint8_t checkSum = 0;
-    extern uint8_t dmaBuffer[19];
+    extern uint8_t bno08x_dmaBuffer[19];
 
-    DL_DMA_disableChannel(DMA, DMA_CH0_CHAN_ID);
-    uint8_t rxSize = 18 - DL_DMA_getTransferSize(DMA, DMA_CH0_CHAN_ID);
+    DL_DMA_disableChannel(DMA, DMA_BNO08X_CHAN_ID);
+    uint8_t rxSize = 18 - DL_DMA_getTransferSize(DMA, DMA_BNO08X_CHAN_ID);
 
     if(DL_UART_isRXFIFOEmpty(UART_BNO08X_INST) == false)
-        dmaBuffer[rxSize++] = DL_UART_receiveData(UART_BNO08X_INST);
+        bno08x_dmaBuffer[rxSize++] = DL_UART_receiveData(UART_BNO08X_INST);
 
     for(int i=2; i<=14; i++)
-        checkSum += dmaBuffer[i];
+        checkSum += bno08x_dmaBuffer[i];
 
-    if((rxSize == 19) && (dmaBuffer[0] == 0xAA) && (dmaBuffer[1] == 0xAA) && (checkSum == dmaBuffer[18]))
+    if((rxSize == 19) && (bno08x_dmaBuffer[0] == 0xAA) && (bno08x_dmaBuffer[1] == 0xAA) && (checkSum == bno08x_dmaBuffer[18]))
     {
-        bno08x_data.index = dmaBuffer[2];
-        bno08x_data.yaw = (int16_t)((dmaBuffer[4]<<8)|dmaBuffer[3]) / 100.0;
-        bno08x_data.pitch = (int16_t)((dmaBuffer[6]<<8)|dmaBuffer[5]) / 100.0;
-        bno08x_data.roll = (int16_t)((dmaBuffer[8]<<8)|dmaBuffer[7]) / 100.0;
-        bno08x_data.ax = (dmaBuffer[10]<<8)|dmaBuffer[9];
-        bno08x_data.ay = (dmaBuffer[12]<<8)|dmaBuffer[11];
-        bno08x_data.az = (dmaBuffer[14]<<8)|dmaBuffer[13];
+        bno08x_data.index = bno08x_dmaBuffer[2];
+        bno08x_data.yaw = (int16_t)((bno08x_dmaBuffer[4]<<8)|bno08x_dmaBuffer[3]) / 100.0;
+        bno08x_data.pitch = (int16_t)((bno08x_dmaBuffer[6]<<8)|bno08x_dmaBuffer[5]) / 100.0;
+        bno08x_data.roll = (int16_t)((bno08x_dmaBuffer[8]<<8)|bno08x_dmaBuffer[7]) / 100.0;
+        bno08x_data.ax = (bno08x_dmaBuffer[10]<<8)|bno08x_dmaBuffer[9];
+        bno08x_data.ay = (bno08x_dmaBuffer[12]<<8)|bno08x_dmaBuffer[11];
+        bno08x_data.az = (bno08x_dmaBuffer[14]<<8)|bno08x_dmaBuffer[13];
     }
     
     uint8_t dummy[4];
     DL_UART_drainRXFIFO(UART_BNO08X_INST, dummy, 4);
 
-    DL_DMA_setDestAddr(DMA, DMA_CH0_CHAN_ID, (uint32_t) &dmaBuffer[0]);
-    DL_DMA_setTransferSize(DMA, DMA_CH0_CHAN_ID, 18);
-    DL_DMA_enableChannel(DMA, DMA_CH0_CHAN_ID);
+    DL_DMA_setDestAddr(DMA, DMA_BNO08X_CHAN_ID, (uint32_t) &bno08x_dmaBuffer[0]);
+    DL_DMA_setTransferSize(DMA, DMA_BNO08X_CHAN_ID, 18);
+    DL_DMA_enableChannel(DMA, DMA_BNO08X_CHAN_ID);
 }
 #endif
 
