c8f179e4bab27fe510b99f114b564c6e1a483c75
Torris Yin <torris.yin@gmail.com>
2025-06-09 00:50:52 +0800

    Update WIT driver

--- Template/Drivers/MSPM0/interrupt.c
+++ Template/Drivers/MSPM0/interrupt.c
@@ -48,7 +48,7 @@ void UART_BNO08X_INST_IRQHandler(void)
 #if defined UART_WIT_INST_IRQHandler
 void UART_WIT_INST_IRQHandler(void)
 {
-    uint8_t checkSum[3] = {0, 0, 0};
+    uint8_t checkSum, packCnt = 0;
     extern uint8_t wit_dmaBuffer[33];
 
     DL_DMA_disableChannel(DMA, DMA_WIT_CHAN_ID);
@@ -57,34 +57,38 @@ void UART_WIT_INST_IRQHandler(void)
     if(DL_UART_isRXFIFOEmpty(UART_WIT_INST) == false)
         wit_dmaBuffer[rxSize++] = DL_UART_receiveData(UART_WIT_INST);
 
-    for(int i=0; i<=2; i++)
-        for(int j=i*11; j<(i+1)*11-1; j++)
-            checkSum[i] += wit_dmaBuffer[j];
-
-    if(rxSize == 33)
+    while(rxSize >= 11)
     {
-        if((wit_dmaBuffer[0] == 0x55) && (wit_dmaBuffer[1] == 0x51) && (checkSum[0] == wit_dmaBuffer[10]))
+        checkSum=0;
+        for(int i=packCnt*11; i<(packCnt+1)*11-1; i++)
+            checkSum += wit_dmaBuffer[i];
+
+        if((wit_dmaBuffer[packCnt*11] == 0x55) && (checkSum == wit_dmaBuffer[packCnt*11+10]))
         {
-            wit_data.ax = (int16_t)((wit_dmaBuffer[3]<<8)|wit_dmaBuffer[2]) / 2.048; //mg
-            wit_data.ay = (int16_t)((wit_dmaBuffer[5]<<8)|wit_dmaBuffer[4]) / 2.048; //mg
-            wit_data.az = (int16_t)((wit_dmaBuffer[7]<<8)|wit_dmaBuffer[6]) / 2.048; //mg
-            wit_data.temperature =  (int16_t)((wit_dmaBuffer[9]<<8)|wit_dmaBuffer[8]) / 100.0; //°C
+            if(wit_dmaBuffer[packCnt*11+1] == 0x51)
+            {
+                wit_data.ax = (int16_t)((wit_dmaBuffer[packCnt*11+3]<<8)|wit_dmaBuffer[packCnt*11+2]) / 2.048; //mg
+                wit_data.ay = (int16_t)((wit_dmaBuffer[packCnt*11+5]<<8)|wit_dmaBuffer[packCnt*11+4]) / 2.048; //mg
+                wit_data.az = (int16_t)((wit_dmaBuffer[packCnt*11+7]<<8)|wit_dmaBuffer[packCnt*11+6]) / 2.048; //mg
+                wit_data.temperature =  (int16_t)((wit_dmaBuffer[packCnt*11+9]<<8)|wit_dmaBuffer[packCnt*11+8]) / 100.0; //°C
+            }
+            else if(wit_dmaBuffer[packCnt*11+1] == 0x52)
+            {
+                wit_data.gx = (int16_t)((wit_dmaBuffer[packCnt*11+3]<<8)|wit_dmaBuffer[packCnt*11+2]) / 16.384; //°/S
+                wit_data.gy = (int16_t)((wit_dmaBuffer[packCnt*11+5]<<8)|wit_dmaBuffer[packCnt*11+4]) / 16.384; //°/S
+                wit_data.gz = (int16_t)((wit_dmaBuffer[packCnt*11+7]<<8)|wit_dmaBuffer[packCnt*11+6]) / 16.384; //°/S
+            }
+            else if(wit_dmaBuffer[packCnt*11+1] == 0x53)
+            {
+                wit_data.roll  = (int16_t)((wit_dmaBuffer[packCnt*11+3]<<8)|wit_dmaBuffer[packCnt*11+2]) / 32768.0 * 180.0; //°
+                wit_data.pitch = (int16_t)((wit_dmaBuffer[packCnt*11+5]<<8)|wit_dmaBuffer[packCnt*11+4]) / 32768.0 * 180.0; //°
+                wit_data.yaw   = (int16_t)((wit_dmaBuffer[packCnt*11+7]<<8)|wit_dmaBuffer[packCnt*11+6]) / 32768.0 * 180.0; //°
+                wit_data.version = (int16_t)((wit_dmaBuffer[packCnt*11+9]<<8)|wit_dmaBuffer[packCnt*11+8]);
+            }
         }
 
-        if((wit_dmaBuffer[11] == 0x55) && (wit_dmaBuffer[12] == 0x52) && (checkSum[1] == wit_dmaBuffer[21]))
-        {
-            wit_data.gx = (int16_t)((wit_dmaBuffer[14]<<8)|wit_dmaBuffer[13]) / 16.384; //°/S
-            wit_data.gy = (int16_t)((wit_dmaBuffer[16]<<8)|wit_dmaBuffer[15]) / 16.384; //°/S
-            wit_data.gz = (int16_t)((wit_dmaBuffer[18]<<8)|wit_dmaBuffer[17]) / 16.384; //°/S
-        }
-
-        if((wit_dmaBuffer[22] == 0x55) && (wit_dmaBuffer[23] == 0x53) && (checkSum[2] == wit_dmaBuffer[32]))
-        {
-            wit_data.roll  = (int16_t)((wit_dmaBuffer[25]<<8)|wit_dmaBuffer[24]) / 32768.0 * 180.0; //°
-            wit_data.pitch = (int16_t)((wit_dmaBuffer[27]<<8)|wit_dmaBuffer[26]) / 32768.0 * 180.0; //°
-            wit_data.yaw   = (int16_t)((wit_dmaBuffer[29]<<8)|wit_dmaBuffer[28]) / 32768.0 * 180.0; //°
-            wit_data.version = (int16_t)((wit_dmaBuffer[31]<<8)|wit_dmaBuffer[30]);
-        }
+        rxSize -= 11;
+        packCnt++;
     }
     
     uint8_t dummy[4];
