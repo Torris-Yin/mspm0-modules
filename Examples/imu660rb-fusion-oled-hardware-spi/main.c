af59a6e88dbdbf4785c4a4fd7f3f266fe7426481
Torris Yin <torris.yin@gmail.com>
2025-07-12 22:46:53 +0800

    Add IMU660RB driver

--- Examples/imu660rb-fusion-oled-hardware-spi/main.c
+++ Examples/imu660rb-fusion-oled-hardware-spi/main.c
@@ -33,25 +33,55 @@
 #include "ti_msp_dl_config.h"
 #include "main.h"
 
+uint8_t oled_buffer[32];
+
 int main(void)
 {
     SYSCFG_DL_init();
     SysTick_Init();
 
     // MPU6050_Init();
-    // OLED_Init();
+    OLED_Init();
     // Ultrasonic_Init();
     // BNO08X_Init();
     // WIT_Init();
     // VL53L0X_Init();
     // LSM6DSV16X_Init();
-    // IMU660RB_Init();
+    IMU660RB_Init();
 
     /* Don't remove this! */
     Interrupt_Init();
 
+    OLED_ShowString(0,7,(uint8_t *)"IMU660RB Demo",8);
+
+    OLED_ShowString(0,0,(uint8_t *)"Pitch",8);
+    OLED_ShowString(0,2,(uint8_t *)" Roll",8);
+    OLED_ShowString(0,4,(uint8_t *)"  Yaw",8);
+
+    OLED_ShowString(16*6,3,(uint8_t *)"Accel",8);
+    OLED_ShowString(17*6,4,(uint8_t *)"Gyro",8);
+
     while (1) 
     {
-        
+        sprintf((char *)oled_buffer, "%-6.1f", euler.angle.pitch);
+        OLED_ShowString(5*8,0,oled_buffer,16);
+        sprintf((char *)oled_buffer, "%-6.1f", euler.angle.roll);
+        OLED_ShowString(5*8,2,oled_buffer,16);
+        sprintf((char *)oled_buffer, "%-6.1f", euler.angle.yaw);
+        OLED_ShowString(5*8,4,oled_buffer,16);
+
+        sprintf((char *)oled_buffer, "%6.0f", acceleration_mg[0]/1000.0);
+        OLED_ShowString(15*6,0,oled_buffer,8);
+        sprintf((char *)oled_buffer, "%6.0f", acceleration_mg[1]/1000.0);
+        OLED_ShowString(15*6,1,oled_buffer,8);
+        sprintf((char *)oled_buffer, "%6.0f", acceleration_mg[2]/1000.0);
+        OLED_ShowString(15*6,2,oled_buffer,8);
+
+        sprintf((char *)oled_buffer, "%6.0f", angular_rate_mdps[0]/1000.0);
+        OLED_ShowString(15*6,5,oled_buffer,8);
+        sprintf((char *)oled_buffer, "%6.0f", angular_rate_mdps[1]/1000.0);
+        OLED_ShowString(15*6,6,oled_buffer,8);
+        sprintf((char *)oled_buffer, "%6.0f", angular_rate_mdps[2]/1000.0);
+        OLED_ShowString(15*6,7,oled_buffer,8);
     }
 }
